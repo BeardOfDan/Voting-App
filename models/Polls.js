29304a0d738e9b00d1a2247238de22e954f28786
Daniel Roberts <BeardOfDan@gmail.com>
2018-04-02 22:33:06 -0700

    (feat) Update the Poll schema

--- models/Polls.js
+++ models/Polls.js
@@ -1,10 +1,12 @@
 const mongoose, { Schema } = require('mongoose');
 
 const pollSchema = new Schema({
-  'createdBy': { 'userID': { 'type': String, required: true }, 'userName': { 'type': String, 'required': true } },
+  'createdBy': { 'userID': { 'type': String, 'required': true }, 'userName': { 'type': String, 'required': true } },
   'name': String, // should be unique (for each user, but multiple users can have the same name for a poll)
   'options': [], // voting options
-  'votes': [] // votes cast
+  'votes': [{ 'option': { 'type': String, 'required': true }, 'voter': { 'type': String, 'required': true } }] // votes cast
+  // votes.voter is either the user ID or IP address (for unauthenticated users)
+  // This prevents a user from voting more than once on the same poll
 });
 
 mongoose.model('poll', pollSchema);
